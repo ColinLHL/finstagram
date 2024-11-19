86ffcd25ca77f734d798c98e2c2c9526f460b720
ColinLHL <colinlhl@proton.me>
2024-11-19 04:40:35 +0000

    include AR and db models for view

--- app/actions.rb
+++ app/actions.rb
@@ -1,55 +1,6 @@
-def humanized_time_ago(mins_ago)
-  if mins_ago >= 60
-    "#{mins_ago / 60} hours ago"
-  else
-    "#{mins_ago} minutes ago"
-  end
-end
-
 # when the request comes in for the path '/', send back to the client, the index.html
 get '/' do
-  # instance vars
-  @finstagram_post_shark = {
-    username: "sharky_j",
-    avatar_url: "https://live.staticflickr.com/65535/52358606250_01c667c5da_w.jpg",
-    photo_url: "https://live.staticflickr.com/65535/52358421508_786aa10e2c_c.jpg",
-    humanized_time_ago: humanized_time_ago(15),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-      username: "sharky_j",
-      text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
-    }]
-  }
+  @finstagram_posts = FinstagramPost.order(created_at: :desc)
 
-  @finstagram_post_whale = {
-    username: "kirk_whalum",
-    avatar_url: "https://live.staticflickr.com/65535/52358421348_f34c7996b1.jpg",
-    photo_url: "https://live.staticflickr.com/65535/52357237337_1cc718f6a7_4k.jpg",
-    humanized_time_ago: humanized_time_ago(65),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-      username: "kirk_whalum",
-      text: "#weekendvibes"
-    }]
-  }
-
-  @finstagram_post_marlin = {
-    username: "marlin_peppa",
-    avatar_url: "https://live.staticflickr.com/65535/52358415933_0a0e6bc35f_3k.jpg",
-    photo_url: "https://live.staticflickr.com/65535/52358494794_f88b160d15_4k.jpg",
-    humanized_time_ago: humanized_time_ago(190),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-      username: "marlin_peppa",
-      text: "lunchtime! ;)"
-    }]
-  }
-
-  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
-
-  # give us the erb method to call
   erb(:index)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
+# model code to represent the records of data in the DB
 class User < ActiveRecord::Base
+
+  # association methods
+  has_many :finstagram_posts
+  has_many :comments
+  has_many :likes
 
 end
